9c298e2ba9987e8901262ff77b31013199b7d4eb
jhonson willyans <jhonsonwillyans28@gmail.com>
2022-05-30 17:12:11 -0300

    💩 adicionado exemplo de render

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -1,3 +1,18 @@
-class HomeController < ApplicationController
+module Api
+    class HomeController < ApplicationController
+        before_action :set_starlinks
+        
+        def index
+            @starlinks =  @service_stars.general
+            render json: @starlinks[112].values[1..5]
+        end
+        
+        private
+        
+        def set_starlinks
+            @service_stars = SpaceLinks.new
+        end
 
+    end
 end
+
